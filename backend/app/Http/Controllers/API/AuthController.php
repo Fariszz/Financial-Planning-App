ad86e2627679da85ec87b3474b60761bd214bd65
Muhammad Faris Hadi Mulyo <ziraflyo@gmail.com>
2022-05-22 14:40:28 +0700

    add route and fix controller auth

--- backend/app/Http/Controllers/API/AuthController.php
+++ backend/app/Http/Controllers/API/AuthController.php
@@ -54,13 +54,12 @@ class AuthController extends Controller
             $request->validate([
                 'name' => ['required', 'string', 'max::255'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-                'password' => $this->passwordRules()
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
             ]);
 
             User::create([
                 'name' => $request->name,
                 'email' => $request->email,
-                'alamat' => $request->alamat,
                 'password' => Hash::make($request->password)
             ]);
 
