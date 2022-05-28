cfa5b5dba90b82418adea09350574f55bf18d850
Muhammad Faris Hadi Mulyo <ziraflyo@gmail.com>
2022-05-28 12:20:37 +0700

    add rich table

---
+++ backend/database/migrations/2022_05_26_162556_create_riches_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRichesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('riches', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('total_harta');
+            $table->integer('total_utang');
+            $table->integer('total_kekayaan_bersih');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('riches');
+    }
+}
