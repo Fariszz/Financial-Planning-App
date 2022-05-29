ef6cb6b7e232a7997cc3fd833beb67515b03363b
Giovanni I.R <gio12619@gmail.com>
2022-05-29 20:57:15 +0700

    transaction

---
+++ mobile/lib/Page/trans.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Transaction extends StatelessWidget {
+  const Transaction({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: 
+      ,
+    );
+  }
+}
