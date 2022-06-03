5de9a569535dbcf23cbca2c948f81d88f38dfd6c
GalihPrev <galihpreviand27@gmail.com>
2022-06-03 19:52:41 +0700

    Update CategoryScrenn.dart

--- mobile/lib/Page/CategoryScrenn.dart
+++ mobile/lib/Page/CategoryScrenn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/Page/LoginScrenn.dart';
 
 class Category extends StatefulWidget {
   const Category({Key? key}) : super(key: key);
@@ -13,11 +14,11 @@ String? icon;
 class _CategoryState extends State<Category> {
   @override
   Widget build(BuildContext context) {
-    List item = [
-      {"nama": "Harta", "icon": "assets/images/dompet.png"},
-      {"nama": "Utang", "icon": "assets/images/export.png"},
-      {"nama": "Pendapatan", "icon": "assets/images/settings.png"},
-    ];
+    // List item = [
+    //   {"nama": "Harta", "icon": "assets/images/dompet.png"},
+    //   {"nama": "Utang", "icon": "assets/images/export.png"},
+    //   {"nama": "Pendapatan", "icon": "assets/images/settings.png"},
+    // ];
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 238, 238, 238),
@@ -49,6 +50,69 @@ class _CategoryState extends State<Category> {
               ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, top: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                    "How Much ?",
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                    decoration: InputDecoration(
+                        hintText: 'Enter Amount', border: InputBorder.none),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, top: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                    "Name Needs",
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                    decoration: InputDecoration(
+                        hintText: 'Enter Needs', border: InputBorder.none),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
           const Padding(
             padding: EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 20),
             child: Text(
@@ -60,17 +124,15 @@ class _CategoryState extends State<Category> {
             scrollDirection: Axis.horizontal,
             child: Row(
                 children: List.generate(
-              item.length,
+              3,
               (index) => GestureDetector(
-                onTap: () {
-                  setState(() {});
-                },
+                onTap: () {},
                 child: Padding(
                   padding: const EdgeInsets.only(left: 10),
                   child: Container(
                     margin: EdgeInsets.only(left: 10),
                     width: 150,
-                    height: 130,
+                    height: 150,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
                         color: Colors.white,
@@ -83,20 +145,41 @@ class _CategoryState extends State<Category> {
                     child: Padding(
                       padding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
                       child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Image.asset(
-                            item[index]["icon"],
-                            width: 50,
+                            index == 0
+                                ? 'assets/images/dompet.png'
+                                : index == 1
+                                    ? 'assets/images/export.png'
+                                    : 'assets/images/settings.png',
+                            // "assets/images/profile.png",
                             height: 50,
+                            width: 50,
                             fit: BoxFit.cover,
                           ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: 40,
+                            ),
+                            child: Text(
+                              index == 0
+                                  ? 'Harta'
+                                  : index == 1
+                                      ? 'Utang'
+                                      : 'Pendapatan',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black),
+                            ),
+                          ),
                           Text(
-                            item[index]["nama"],
+                            "Loren Ipsum",
                             style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w600,
+                                fontSize: 10,
+                                fontWeight: FontWeight.w400,
                                 color: Colors.black),
                           ),
                         ],
@@ -106,6 +189,32 @@ class _CategoryState extends State<Category> {
                 ),
               ),
             )),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 70, horizontal: 30),
+            // padding:
+            //     EdgeInsets.only(top: 50, left: 30, right: 30),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Continue",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xff7F3DFF),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                minimumSize: const Size(double.infinity, 50),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40,
+                ),
+              ),
+            ),
           )
         ],
       )),
