750d006a40ceccee43f7a72b7b5d37e5333906a6
GalihPrev <galihpreviand27@gmail.com>
2022-06-08 09:12:14 +0700

    fix conflict

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,39 +1,24 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-
-import 'package:mobile/Page/InputHutang.dart';
+import 'package:mobile/Page/HomeScrenn.dart';
+import 'package:mobile/Page/InputMoneyScreen.dart';
 import 'package:mobile/Page/InputTabungan.dart';
 import 'package:mobile/Page/LoginScrenn.dart';
-import 'package:mobile/Page/RegisterScrenn.dart';
-import 'package:mobile/Page/CategoryScrenn.dart';
-import 'package:mobile/Page/profile.dart';
+import 'package:mobile/Page/stats.dart';
 import 'package:mobile/Page/trans.dart';
-import 'package:mobile/providers/auth_provider.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => AuthProvider()),
-      ],
-      child: MaterialApp(
-        routes: {
-          '/': (context) => InputHutang(),
-          '/register': (context) => RegisterScreen(),
-          '/trans': (context) => Transaction(),
-          '/home': (context) => Profile(),
-        },
-      ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: Home(),
     );
   }
 }
