025ca0500886e88f942553da1b5d03be46a4bcea
GalihPrev <galihpreviand27@gmail.com>
2022-06-08 09:32:07 +0700

    fix conflict

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/Page/HomeScrenn.dart';
-import 'package:mobile/Page/InputMoneyScreen.dart';
 import 'package:mobile/Page/InputTabungan.dart';
 import 'package:mobile/Page/LoginScrenn.dart';
 import 'package:mobile/Page/stats.dart';
