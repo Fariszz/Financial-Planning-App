517d746d62a153c51a31ff5986ceb68bdad94fbb
GalihPrev <galihpreviand27@gmail.com>
2022-06-15 14:42:12 +0700

    Fix Conflict

--- mobile/lib/Page/CategoryScrenn.dart
+++ mobile/lib/Page/CategoryScrenn.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/Page/input_Harta.dart';
+
 import 'package:mobile/Page/pages.dart';
 
 class Category extends StatefulWidget {
--- mobile/lib/Page/pages.dart
+++ mobile/lib/Page/pages.dart
@@ -3,10 +3,11 @@ export 'trans.dart';
 export 'profile.dart';
 export 'CategoryScrenn.dart';
 export 'Input_Hutang.dart';
+export 'input_harta.dart';
 
 export 'home.dart';
 export 'input_expend.dart';
 export 'Input_Hutang.dart';
 export 'input_ideal.dart';
 export 'input_pendapatan.dart';
-export 'inputHarta.dart';
+
