c50ba8a132da2ad98eb358c5dd19a6ddd4372940
Giovanni I.R <gio12619@gmail.com>
2022-06-15 10:49:00 +0700

    update profile

--- mobile/lib/Page/profile.dart
+++ mobile/lib/Page/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/Widgets/Navigator_bar.dart';
+import 'package:mobile/widgets/appbar.dart';
 
 class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
@@ -7,140 +8,155 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          SizedBox(height: 50),
-          Row(
+          Positioned(
+            child: AppBarAll(),
+            bottom: 0,
+          ),
+          Column(
             children: [
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Image(
-                  image: AssetImage('assets/images/profile.png'),
-                ),
-              ),
-              // SizedBox(
-              //   width: 20,
-              // ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
                 children: [
-                  Text(
-                    'Username',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Image(
+                      image: AssetImage('assets/images/profile.png'),
                     ),
                   ),
-                  Text(
-                    'Iriana Saliha',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w600,
-                    ),
+                  // SizedBox(
+                  //   width: 20,
+                  // ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Username',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        'Iriana Saliha',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
                   ),
+                  SizedBox(width: 40),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                    ),
+                    onPressed: () {},
+                    child: Image(
+                      image: AssetImage('assets/images/edit.png'),
+                    ),
+                  )
                 ],
               ),
-              SizedBox(width: 40),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white,
+              Container(
+                margin: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/dompet.png'),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'My Wallet',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )),
+                  ],
                 ),
-                onPressed: () {},
-                child: Image(
-                  image: AssetImage('assets/images/edit.png'),
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/settings.png'),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Settings',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              )
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/export.png'),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Export Data',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/logout.png'),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Logout',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              // Stack(
+              //   children: [
+              //     Positioned(
+              //       child: AppBarAll(),
+              //       bottom: 10,
+              //     )
+              //   ],
+              // )
             ],
-          ),
-          Container(
-            margin: EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Image(
-                  image: AssetImage('assets/images/dompet.png'),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'My Wallet',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    )),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Image(
-                  image: AssetImage('assets/images/settings.png'),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Settings',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Image(
-                  image: AssetImage('assets/images/export.png'),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Export Data',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    )),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Image(
-                  image: AssetImage('assets/images/logout.png'),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Logout',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                )
-              ],
-            ),
           ),
         ],
       ),
